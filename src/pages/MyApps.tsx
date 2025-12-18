3b3621435aceaf8daf07d25507256cb4236aeb51
Ehsan-saradar <ehsan.saradar@gmail.com>
2025-12-18 12:49:15 +0330

    Refactor Automations and MyApps pages UI and actions (#99)
    * Refactor Automations and MyApps pages UI and actions
    
    * Update uninstall flow to navigate after success

--- src/pages/MyApps.tsx
+++ src/pages/MyApps.tsx
@@ -1,63 +1,21 @@
-import { Dropdown, Empty } from "antd";
+import { Empty } from "antd";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useTheme } from "styled-components";
 
-import { useAntd } from "@/hooks/useAntd";
-import { CirclePlusIcon } from "@/icons/CirclePlusIcon";
-import { DotGridVerticalIcon } from "@/icons/DotGridVerticalIcon";
-import { TrashIcon } from "@/icons/TrashIcon";
+import { ChevronRightIcon } from "@/icons/ChevronRightIcon";
 import { Spin } from "@/toolkits/Spin";
 import { HStack, Stack, VStack } from "@/toolkits/Stack";
-import { getMyApps, uninstallApp } from "@/utils/api";
-import { modalHash } from "@/utils/constants";
+import { getMyApps } from "@/utils/api";
 import { routeTree } from "@/utils/routes";
 import { App } from "@/utils/types";
 
-type StateProps = {
-  loading: boolean;
-  apps: App[];
-};
+type StateProps = { loading: boolean; apps: App[] };
 
 export const MyAppsPage = () => {
   const [state, setState] = useState<StateProps>({ loading: true, apps: [] });
   const { loading, apps } = state;
-  const { messageAPI, modalAPI } = useAntd();
   const colors = useTheme();
-
-  const handleUninstall = (app: App) => {
-    modalAPI.confirm({
-      title: "Are you sure you want to uninstall this app?",
-      okText: "Yes",
-      okType: "danger",
-      cancelText: "No",
-      onOk() {
-        setState((prevState) => ({ ...prevState, loading: true }));
-
-        uninstallApp(app.id)
-          .then(() => {
-            setState((prevState) => ({
-              ...prevState,
-              apps: prevState.apps.filter((a) => a.id !== app.id),
-              loading: false,
-            }));
-
-            messageAPI.open({
-              type: "success",
-              content: "App successfully uninstalled",
-            });
-          })
-          .catch(() => {
-            setState((prevState) => ({ ...prevState, loading: false }));
-
-            messageAPI.open({
-              type: "error",
-              content: "App uninstallation failed",
-            });
-          });
-      },
-    });
-  };
 
   useEffect(() => {
     getMyApps({})
@@ -97,7 +55,10 @@ export const MyAppsPage = () => {
           <VStack $style={{ gap: "16px" }}>
             {apps.map((app) => (
               <HStack
+                as={Link}
                 key={app.id}
+                state={true}
+                to={routeTree.automations.link(app.id)}
                 $style={{
                   alignItems: "center",
                   border: `1px solid ${colors.borderLight.toHex()}`,
@@ -108,19 +69,17 @@ export const MyAppsPage = () => {
                 }}
               >
                 <HStack $style={{ alignItems: "center", gap: "16px" }}>
-                  <Stack as={Link} to={routeTree.app.link(app.id)}>
-                    <Stack
-                      as="img"
-                      alt={app.title}
-                      src={app.logoUrl}
-                      $style={{
-                        borderRadius: "12px",
-                        display: "block",
-                        height: "56px",
-                        width: "56px",
-                      }}
-                    />
-                  </Stack>
+                  <Stack
+                    as="img"
+                    alt={app.title}
+                    src={app.logoUrl}
+                    $style={{
+                      borderRadius: "12px",
+                      display: "block",
+                      height: "56px",
+                      width: "56px",
+                    }}
+                  />
                   <VStack
                     $style={{
                       alignItems: "flex-start",
@@ -128,11 +87,7 @@ export const MyAppsPage = () => {
                       justifyContent: "center",
                     }}
                   >
-                    <Stack
-                      as={Link}
-                      to={routeTree.app.link(app.id)}
-                      $style={{ fontSize: "17px", lineHeight: "20px" }}
-                    >
+                    <Stack $style={{ fontSize: "17px", lineHeight: "20px" }}>
                       {app.title}
                     </Stack>
                     <Stack
@@ -146,47 +101,10 @@ export const MyAppsPage = () => {
                     </Stack>
                   </VStack>
                 </HStack>
-                <Dropdown
-                  menu={{
-                    items: [
-                      {
-                        icon: <CirclePlusIcon />,
-                        key: "1",
-                        label: (
-                          <Link
-                            to={`${routeTree.automations.link(app.id)}${
-                              modalHash.policy
-                            }`}
-                          >
-                            Add Automation
-                          </Link>
-                        ),
-                      },
-                      {
-                        danger: true,
-                        icon: <TrashIcon />,
-                        key: "2",
-                        label: "Uninstall",
-                        onClick: () => {
-                          handleUninstall(app);
-                        },
-                      },
-                    ],
-                  }}
-                  placement="bottomRight"
-                >
-                  <VStack
-                    $style={{
-                      backgroundColor: colors.bgSecondary.toHex(),
-                      borderRadius: "50%",
-                      cursor: "pointer",
-                      fontSize: "16px",
-                      padding: "12px",
-                    }}
-                  >
-                    <DotGridVerticalIcon />
-                  </VStack>
-                </Dropdown>
+                <Stack
+                  as={ChevronRightIcon}
+                  $style={{ flex: "none", fontSize: "16px" }}
+                />
               </HStack>
             ))}
           </VStack>
